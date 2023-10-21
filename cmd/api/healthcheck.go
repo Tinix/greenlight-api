f1b5e6c783d371161dc3ddad4a268c53068cb7f0
Daniel Tinivella <dtinivella@gmail.com>
2023-10-21 19:03:02 -0400

    add a basic HTTP Server

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,17 @@
+// healthcheck.go
+// Copyright (C) 2023 tinix <tinix@debian>
+//
+// Distributed under terms of the MIT license.
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tinix/greenlight
 
 go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
