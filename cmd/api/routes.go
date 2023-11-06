c8c8c227a639fb914890e165954741bd23e464d8
Daniel Tinivella <dtinivella@gmail.com>
2023-11-06 01:25:41 -0300

    add new config file to errors 500

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,11 +16,12 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/api/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/api/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/api/v1/movies/:id", app.showMovieHandler)
 
 	return router
 }
-
-
--- pkg/data/movies.go
+++ pkg/data/movies.go
@@ -8,6 +8,8 @@
 package data
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -19,4 +21,31 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty,string"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+func (m Movie) MarshalJSON() ([]byte, error) {
+
+	var runtime string
+
+	if m.Runtime != 0 {
+
+		runtime = fmt.Sprintf("%d mins", m.Runtime)
+
+	}
+
+	type MovieAlias Movie
+
+	aux := struct {
+		MovieAlias
+
+		Runtime string `json:"runtime,omitempty"`
+	}{
+
+		MovieAlias: MovieAlias(m),
+
+		Runtime: runtime,
+	}
+
+	return json.Marshal(aux)
+
 }
